a681fa6e309c552f93848ed0369e20027232f6cb
kw3ngu <kwengu72@gmail.com>
2025-01-10 14:12:44 +0300

    working calculator

--- main.js
+++ main.js
@@ -1,10 +1,61 @@
-let number = document.querySelectorAll(".number");
+let currentNum = "";
+let previousNum = "";
+let operator = "";
 
-number.forEach((btn) => {
+const numberButtons = document.querySelectorAll(".number");
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+const currentDisplay = document.querySelector(".currentDisplay");
+
+const prevDisplay = document.querySelector(".prevDisplay");
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click",calculate)
+
+const clear = document.querySelector(".clear");
+
+
+numberButtons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        getNumber(e.target.textContent)
+        getNumber(e.target.textContent);
     })
 })
-function getNumber(num){
-    console.log(num)
+function getNumber(num){4
+    currentNum += num;
+    currentDisplay.textContent = currentNum;
+}
+
+operatorButtons.forEach((btn) =>{
+    btn.addEventListener("click", (e) => {
+        getOperator(e.target.textContent);
+    })
+})
+function getOperator(op){
+    operator = op;
+    previousNum = currentNum;
+    prevDisplay.textContent = previousNum + " " + operator;
+    currentNum = "";
+    currentDisplay.textContent = "";
+}
+function calculate(){
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    switch(operator){
+        case "+":
+            previousNum += currentNum;
+            break;
+        case "*":
+            previousNum *= currentNum;
+            break;
+        case "/":
+            previousNum /= currentNum;
+            break;
+        case "-":
+            previousNum -= currentNum;
+            break;
+    }
+    prevDisplay.textContent = "";
+    currentDisplay.textContent = previousNum;
 }
